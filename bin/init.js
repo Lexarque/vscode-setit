0f7789e9b48243e9aee0dd467a7d0c880f26a08b
Lexarque <satrialdyfarizki06@gmail.com>
2023-09-08 08:39:51 +0700

    Add condition to be able to overwrite config.json

--- bin/init.js
+++ bin/init.js
@@ -1,29 +1,70 @@
 import fs from "fs/promises";
 import inquirer from "inquirer";
 import os from "os";
+import { fileExists } from "../lib/helper.js";
 
 async function main() {
-  await fs.writeFile("./lib/presets.json", []);
+  if (fileExists("./config.json")) {
+    const answer = await inquirer.prompt([
+      {
+        type: "input",
+        message:
+          "config.json already exists, do you want to overwrite it? Warning: THIS WILL DELETE YOUR CONFIG. (y/n):",
+        name: "answer",
+      },
+    ]);
 
-  const OS = os.platform();
+    if (answer.answer === "n") {
+      return;
+    }
 
-  const extensionsFilePath = await inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter the path to the extensions file:",
-      name: "extensionsFilePath",
-      default:
-        OS === "win32"
-          ? process.env.USERPROFILE
-          : process.env.HOME + "/.vscode/extensions",
-    },
-  ]);
+    console.log("Overwriting config.json file.");
 
-  const configDefault = { EXTENSIONS_FOLDER_PATH: extensionsFilePath.extensionsFilePath };
+    await fs.unlink("./config.json");
+    await createConfigFile();
+  } else {
+    // Create config.json & presets.json if it doesn't exist
+    await createConfigFile();
+    await createPresetsFile([]);
+  }
 
-  await fs.writeFile("config.json", JSON.stringify(configDefault, null, 2));
+  console.log(
+    "Successfully created/configured config.json & presets.json files."
+  );
+}
 
-  console.log("Successfully created config.json & presets.json file.");
+async function createConfigFile() {
+  try {
+    const OS = os.platform();
+
+    const extensionsFilePath = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Enter the path to the extensions file:",
+        name: "extensionsFilePath",
+        default:
+          OS === "win32"
+            ? process.env.USERPROFILE
+            : process.env.HOME + "/.vscode/extensions",
+      },
+    ]);
+
+    const configDefault = {
+      EXTENSIONS_FOLDER_PATH: extensionsFilePath.extensionsFilePath,
+    };
+
+    await fs.writeFile("config.json", JSON.stringify(configDefault, null, 2));
+  } catch (error) {
+    console.error("Error creating config.json:", error);
+  }
+}
+
+async function createPresetsFile() {
+  try {
+    await fs.writeFile("presets.json", []);
+  } catch (error) {
+    console.error("Error creating presets.json:", error);
+  }
 }
 
 main();
